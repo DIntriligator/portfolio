b9cfe50ba3fcbe6a173a45cf897884ea6fb02389
Dan Intriligator <DIntriligator@smartequip.com>
2020-07-29 21:10:38 -0400

    creates showcase page

--- src/app/work/work.component.ts
+++ src/app/work/work.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { WORK_LISTING } from './work-listing';
 
 @Component({
   selector: 'app-work',
@@ -6,11 +7,14 @@ import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
   styleUrls: ['./work.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class WorkComponent implements OnInit {
+export class WorkComponent {
+
+  workListing = WORK_LISTING;
 
   constructor() { }
 
-  ngOnInit() {
+  onExternalNavigation(link: string) {
+    window.open(link, '_blank');
   }
 
 }
--- src/app/work/work.module.ts
+++ src/app/work/work.module.ts
@@ -3,13 +3,17 @@ import { CommonModule } from '@angular/common';
 import { WorkComponent } from './work.component';
 import { UISiteWrapperModule } from '../../../projects/di-ui/src/lib/components/site-wrapper/site-wrapper.module';
 import { UICardModule } from '../../../projects/di-ui/src/lib/components/card/card.module';
+import { UIButtonAreaModule } from '../../../projects/di-ui/src/lib/components/button-area/button-area.module';
+import { UIButtonModule } from '../../../projects/di-ui/src/lib/components/button/button.module';
 
 @NgModule({
   declarations: [WorkComponent],
   imports: [
     CommonModule,
     UISiteWrapperModule,
-    UICardModule
+    UICardModule,
+    UIButtonAreaModule,
+    UIButtonModule
   ]
 })
 export class WorkModule { }
